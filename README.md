2013a829010e35396d4016e969aeeca15c30b30a
gianmarcodonetti <gianmarco.donetti@yahoo.it>
2022-12-05 09:52:41 +0100

    first commit

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-# README
+# door2door
 
-[![Build Status](https://travis-ci.org/gianmarcodonetti/pyspark-ingestion.svg?branch=master)](https://travis-ci.org/gianmarcodonetti/pyspark-ingestion)
-[![Test Coverage](https://codecov.io/gh/gianmarcodonetti/pyspark-ingestion/branch/master/graph/badge.svg)](https://codecov.io/gh/gianmarcodonetti/pyspark-ingestion)
+[![Build Status](https://app.travis-ci.com/gianmarcodonetti/door2door.svg?branch=main)](https://app.travis-ci.com/github/gianmarcodonetti/door2door/builds)
+[![codecov](https://codecov.io/gh/gianmarcodonetti/door2door/branch/main/graph/badge.svg?token=PF72U1MVOP)](https://codecov.io/gh/gianmarcodonetti/door2door)
 
 
-First services developed:
-1. Batch ingestion for a single table
-2. Batch ingestion for all tables in a defined system
+## Introduction
+**door2door** collects the live position of all vehicles in its fleet in real-time via a GPS sensor in each
+vehicle. These vehicles run in operating periods that are managed by **door2door**’s operators. An API is
+responsible for collecting information from the vehicles and place it on an S3 bucket, in raw format, to
+be consumed.
+
+The goal of this challenge is to automate the build of a simple yet scalable data lake and data warehouse
+that will enable our BI team to answer questions like:
+
+_What is the average distance traveled by our vehicles during an operating period?_
+
+We would like to ask you to develop a solution that:
+1. Fetches the data from the bucket on a daily basis and stores it on a data lake;
+2. Processes and extracts the main events that occurred during operating periods;
+3. Store the transformed data on a data warehouse. The data warehouse should be SQL-queriable
+(SQL database or using something like AWS Athena).
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import os
 import re
 
-import pip
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 from setuptools.command.install import install
+
+try:
+    from pip._internal import main
+except ImportError:
+    import pip.main as main
 
 try:  # for pip >= 10
     from pip._internal.req import parse_requirements
 except ImportError:  # for pip <= 9.0.3
     from pip.req import parse_requirements
-PACKAGE_NAME = os.getcwd().split(os.sep)[-1].replace('-', '_')
 
-with open(os.path.join(os.path.dirname(__file__), PACKAGE_NAME, 'version.py')) as v_file:
+with open(os.path.join(os.path.dirname(__file__), 'door2door', 'version.py')) as v_file:
     VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
 
 
@@ -22,18 +25,23 @@ class OverrideInstall(install):
 
     def run(self):
         # install requirements first
-        install_reqs = parse_requirements(
-            './requirements/common.txt', session=False)
-        reqs = [str(ir.req) for ir in install_reqs]
+        install_reqs = parse_requirements('./requirements/common.txt', session=False)
+        # Generator must be converted to list, or we will only have one chance to read each element,
+        # meaning that the first requirement will be skipped.
+        requirements = list(install_reqs)
+        try:
+            reqs = [str(ir.req) for ir in requirements]
+        except:
+            reqs = [str(ir.requirement) for ir in requirements]
         for req in reqs:
-            pip.main(["install", req])
+            main(["install", req])
 
         install.run(self)
 
 
 config = {
-    'name': PACKAGE_NAME,
-    'description': 'Python package for Spark Ingestion',
+    'name': 'decc',
+    'description': 'door2door repository',
     'author': 'G. Donetti',
     'url': '',
     'download_url': '',
